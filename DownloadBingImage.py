340375894b1b5385d13e9b107f527e668a29354d
zasilla <e-flying@hotmail.com>
2019-01-05 12:17:55 +0800

    已有文件不会重新下载

--- DownloadBingImage.py
+++ DownloadBingImage.py
@@ -33,29 +33,33 @@ def pars_page_detail(html):
 
 
 def download_img(url):
+    #检查文件夹路径是否存在，不存在的话创建文件夹，否则跳过
     img_dir = os.getcwd() + "\images\\"
     if os.path.exists(img_dir):
         pass
     else:
         os.mkdir(img_dir)
     image_name = re.findall(r'/([^/]+)$', url)
-    f = open(img_dir + image_name[0], "wb")
-    try:
-        conn = requests.get(url)
-    except RequestException:
-        print(url)
-        print("请重新下载")
-    f.write(conn.content)
-    f.close()
-    print(url, "保存成功")
+    #检查文件是否存在，如果存在则跳过
+    if not os.path.exists(img_dir + image_name[0]):
+        with open(img_dir + image_name[0], "wb") as f:
+            try:
+                conn = requests.get(url)
+            except RequestException:
+                print(url)
+                print("请重新下载")
+            f.write(conn.content)
+            f.close()
+            print(url, "保存成功")
+    else:
+        print(image_name[0] + "已经下载")
 
 
 def main():
     for idx in range(7):
         html = get_page_detail(idx)
         for url in pars_page_detail(html):
-            #download_img(url)
-            print(url)
+            download_img(url)
 
 
 
